26c7219b5a3501aa9d1103e317ef3479157915b2
Katherine Wang <kwang004@gmail.com>
2019-07-05 13:54:05 -0700

    Setup Enzyme and Jest

---
+++ client/src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+
+// eslint-disable-next-line no-undef
+describe('First React component test with Enzyme', () => {
+  // eslint-disable-next-line no-undef
+  it('renders without crashing', () => {
+    shallow(<App />);    
+  });
+});
