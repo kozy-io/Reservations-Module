ed3a44881a5dbc60b18e2066a92ae46b888c5d3e
Katherine Wang <kwang004@gmail.com>
2019-07-06 12:50:32 -0700

    When the component mounts, a get request is made to fetch the data of a random listing - State is subsequently updated with the listing info

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+
 import Calendar from './Calendar.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      listing_id: 0,
+      base_rate: 0,
+      currency: 'USD',
+      extra_guest_cap: 0,
+      extra_guest_charge: 0,
+      id: 0,
+      local_tax: 0.09,
+      max_guests: 0,
+      min_stay: 0,
+      review_count: 0,
+      star_rating: 0,
     };
+
+    this.getListing = this.getListing.bind(this);
   }
 
   componentDidMount() {
-    // get a random listing and pass it down to Calendar
+    this.getListing();
   }
+
+  getListing() {
+    const random = Math.floor(Math.random() * 100);
+    axios.get(`/listing/${random}`)
+      .then((response) => {
+        const { base_rate, currency, extra_guest_cap, extra_guest_charge, id, local_tax, max_guests,
+          min_stay, review_count, star_rating } = response.data;
+        
+        this.setState({
+          base_rate, currency, extra_guest_cap, extra_guest_charge, id, local_tax, max_guests,
+          min_stay, review_count, star_rating
+        })
+      });
+  }
+
 
   render() {
     return (
