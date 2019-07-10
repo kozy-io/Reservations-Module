8e45e6fc843078fbed2798920575d7338b623633
Katherine Wang <kwang004@gmail.com>
2019-07-10 12:40:07 -0700

    Added test coverage for Calendar component

--- client/src/components/__tests__/Calendar.test.js
+++ client/src/components/__tests__/Calendar.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Calendar from '../Calendar';
+import moment from 'moment';
+
 
 
 // eslint-disable-next-line no-undef
@@ -26,28 +28,123 @@ describe('Calendar Structure', () => {
   });
 
   it('should disable pointers (click functionality) for dates that are in the past', () => {
+    let prevMo = moment().subtract(1, 'months');
+    let prevMoDays = prevMo.daysInMonth();
+
+    const wrapper = shallow(<Calendar />);
+    const arrow = wrapper.find('svg.svg-left-arrow');
+    arrow.simulate('click');
+    const disabled = wrapper.find('td.week-days-disabled-normal');
+    
+    expect(disabled.length).toBe(prevMoDays);
 
   });
 
   it('should disable pointers (click functionality) for dates that are reserved (based on the listing displayed)', () => {
+    const wrapper = shallow(<Calendar id={1} view={'in'} />);
+    const mockReserveDates = [1, 2, 3, 4, 5];
+    wrapper.setState({ reserved: mockReserveDates });
+
+    const arrow = wrapper.find('svg.svg-right-arrow');
+    arrow.simulate('click');
+
+    const days = wrapper.find('td.week-days-disabled-normal');
+
+    expect(days.length).toBe(mockReserveDates.length);
+  });
+
+  it('clicking on the right arrow should change the calendar month to the next month (and year if applicable)', () => {
+    const wrapper = shallow(<Calendar />);
+    const today = moment();
+    const arrowMove = 10;
+
+    const future = moment(today).add(arrowMove, 'months');
+    const futureMonth = future.month();
+    const futureYear = future.year();
+
+    const arrow = wrapper.find('svg.svg-right-arrow');
+    
+    for (var i = 0; i < arrowMove; i++) {
+      arrow.simulate('click');
+    }
+
+    expect(wrapper.state().currentMonth).toBe(futureMonth);
+    expect(wrapper.state().currentYear).toBe(futureYear);
 
   });
+
+
+  it('clicking on the left arrow should change the calendar month to the previous month (and year if applicable)', () => {
+    const wrapper = shallow(<Calendar />);
+    const today = moment();
+    const arrowMove = 10;
+
+    const past = moment(today).subtract(arrowMove, 'months');
+    const pastMonth = past.month();
+    const pastYear = past.year();
+
+    const arrow = wrapper.find('svg.svg-left-arrow');
+    
+    for (var i = 0; i < arrowMove; i++) {
+      arrow.simulate('click');
+    }
+
+    expect(wrapper.state().currentMonth).toBe(pastMonth);
+    expect(wrapper.state().currentYear).toBe(pastYear);
+  });
+
 
 });
 
 describe('Calendar Functionality', () => {
-
-  // eslint-disable-next-line no-undef
   it('clearing the calendar should reset all previously selected dates, and revert to the initial view', () => {
+    const wrapper = shallow(<Calendar id={1} view={'in'} clearSelectedDates={() => {}}/>);
 
+    let currentMonth = moment().month();
+
+    const arrow = wrapper.find('svg.svg-right-arrow');
+    arrow.simulate('click');
+    arrow.simulate('click');
+    arrow.simulate('click');
+    arrow.simulate('click');
+
+    const clear = wrapper.find('button');
+    clear.simulate('click', {
+      preventDefault: () => {
+      }
+    });
+
+    expect(wrapper.state().currentMonth).toBe(currentMonth);
   });
 
-  it('clicking on a check-in date with an invalid check-out date should render all days disabled', () => {
+  it('clicking on a check-in date should render all previous dates disabled (to prevent check out before check in)', () => {
+    const wrapper = shallow(<Calendar view={'in'} getSelectedDates={() => {}} />);
+    const table = wrapper.find('td').last();
+    table.simulate('click');
 
+    let daysInMonth = moment().daysInMonth();
+
+    let disabled = wrapper.find('td.week-days-disabled-normal');
+
+    expect(disabled.length).toBe(daysInMonth - 1); // one item should be "selected" and not disabled.
   });
 
-  it('clicking on a check-out date with an invalid check-in date should render all days disabled', () => {
+  it('clicking on a check-out date should render all future dates disabled (to prevent check in after check out)', () => {
+    const wrapper = shallow(<Calendar view={'out'} getSelectedDates={() => {}} />);
+    wrapper.setState({reserved: []});
 
+    // go to next month (so no past days are interfering)
+    const arrow = wrapper.find('svg.svg-right-arrow');
+    arrow.simulate('click');
+    
+    // now, find cell 19 (arbitrary) and click it (nodes are indexed from 0)
+    const table = wrapper.find('td.week-days-active-normal').at(18);
+    table.simulate('click');
+    
+    // now, find the number of disabled cells which should be 20-31 (since we are in August) 
+    let disabled = wrapper.find('td.week-days-disabled-normal');
+
+    expect(disabled.length).toBe(12);
   });
 
 });
